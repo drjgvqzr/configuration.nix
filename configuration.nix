1ffe7f3da9c4da2c08a618ed1157c403a56df170
soma <ligma@mailbox.org>
2026-01-18 15:18:34 +0100

    The `resolved` configuration shifted from top-level options to a nested `settings.Resolve` attribute block

--- configuration.nix
+++ configuration.nix
@@ -374,10 +374,12 @@
         playerctld.enable = true;
         resolved = {
             enable = true;
-            dnssec = "true";
-            domains = ["~."];
-            fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-            dnsovertls = "true";
+            settings.Resolve = {
+                DNSOverTLS = "true";
+                DNSSEC = "true";
+                Domains = ["~."];
+                FallbackDNS = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+            };
         };
         syncthing = {
             enable = true;
