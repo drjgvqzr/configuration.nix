36f53545d806b0e72f1ceb3d61614757cc53c9bd
soma <ligma@mailbox.org>
2026-02-22 13:01:46 +0100

    Changed the mpv sponsorblock binding from `sponsorblock/sponsorblock` to `sponsorblock_minimal/sponsorblock`

--- configuration.nix
+++ configuration.nix
@@ -757,7 +757,7 @@
                 ENTER = "playlist-next";
                 "ctrl+c" = "quit-watch-later";
                 SPACE = "cycle pause";
-                b = "script-binding sponsorblock/sponsorblock";
+                b = "script-binding sponsorblock_minimal/sponsorblock";
             };
         };
         programs.neovim = {
