c93daced22b5693d7d2c28911904a2f45f3182ed
soma <ligma@mailbox.org>
2026-02-22 20:12:55 +0100

    System state version changed from unspecified to 26.05

--- configuration.nix
+++ configuration.nix
@@ -302,7 +302,7 @@
     ];
     system = {
         autoUpgrade.enable = true;
-        #stateVersion = "24.11";
+        stateVersion = "26.05";
     };
     systemd.sleep.extraConfig = "HibernateDelaySec=1h";
     services = {
