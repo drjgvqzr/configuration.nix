aa096c763e8cbe63ebd888011ecae44c3e151813
soma <ligma@mailbox.org>
2026-04-05 14:56:05 +0200

    Blocked 4chan.org via extraHosts and added Gooseworx and GLITCH YouTube feeds to newsboat

--- configuration.nix
+++ configuration.nix
@@ -211,6 +211,7 @@
         graphics.enable = true;
     };
     networking = {
+        extraHosts = "0.0.0.0 4chan.org";
         dhcpcd.enable = false;
         hostName = "laptop";
         nameservers = ["9.9.9.9#dns.quad9.net"];
