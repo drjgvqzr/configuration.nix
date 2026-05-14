24d496871cfc9e5070340043297a2b9f399f12ed
soma <ligma@mailbox.org>
2026-05-14 23:02:33 +0200

    Added `documentation.man.cache.enable = true`

--- configuration.nix
+++ configuration.nix
@@ -179,6 +179,7 @@
     console.useXkbConfig = true;
     documentation.man.mandoc.enable = true;
     documentation.man.man-db.enable = false;
+    documentation.man.cache.enable = true;
     environment = {
         binsh = "${pkgs.dash}/bin/dash";
         defaultPackages = [];
