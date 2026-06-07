ab2abea81340a999657938ce8227f1a3f31e0073
soma <ligma@mailbox.org>
2026-06-07 18:42:48 +0200

    Zathura’s delete mapping now also kills the process showing the removed file

--- configuration.nix
+++ configuration.nix
@@ -999,8 +999,7 @@
         programs.zathura = {
             enable = true;
             mappings = {
-                "<BackSpace>" = "exec \"rm '$FILE'\"";
-                "<Delete>" = "exec \"rm '$FILE'\"";
+                "<BackSpace>" = "exec \"rm '$FILE' && pkill -f 'zathura.*$FILE'\"";
                 "w" = "scroll up";
                 "a" = "scroll left";
                 "r" = "scroll down";
