ebd68c9fe37d1189f4f615f76be8407839e4e4be
soma <ligma@mailbox.org>
2026-06-10 00:57:24 +0200

    The DEL keybinding in mpv now also advances to the next playlist item after deleting the current file

--- configuration.nix
+++ configuration.nix
@@ -735,7 +735,7 @@
                 ];
                 bindings = {
                     "Shift+RIGHT" = "seek 1";
-                    "DEL" = "run gtrash put \${path}";
+                    "DEL" = "run gtrash put \${path} ; playlist-next";
                     "Shift+LEFT" = "seek -1";
                     RIGHT = "seek 5";
                     LEFT = "seek -5";
