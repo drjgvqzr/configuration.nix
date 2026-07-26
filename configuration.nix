087e17fc1809e6e6401b053f21f979a9a4fcf7f7
soma <ligma@mailbox.org>
2026-07-26 05:23:22 +0200

    Fixed a command substitution syntax error in the `systemd.services.ticker` script by replacing parentheses with a dollar sign and parentheses

--- configuration.nix
+++ configuration.nix
@@ -332,7 +332,7 @@
     };
     systemd.sleep.settings.Sleep.HibernateDelaySec = "3h";
     systemd.services.ticker = {
-        script = ''curl -s https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=WEBN.DEX&apikey=(cat /home/soma/dx/nixos/misc/secrets/alphavantage) | jq -r '."Global Quote"."05. price"' | sed 's/0*$/€/' > /tmp/webn '';
+        script = ''curl -s https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=WEBN.DEX&apikey=$(cat /home/soma/dx/nixos/misc/secrets/alphavantage) | jq -r '."Global Quote"."05. price"' | sed 's/0*$/€/' > /tmp/webn '';
         startAt = "daily";
     };
     services = {
