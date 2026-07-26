89bf6f8cb51bd42bdbc654afb8a7f3f365567d8b
soma <ligma@mailbox.org>
2026-07-26 05:42:49 +0200

    Window opacity disabled and set to fully opaque

--- sway.nix
+++ sway.nix
@@ -49,7 +49,8 @@
                 titlebar = false;
                 commands = [
                     {
-                        command = "opacity 0.75";
+                        #command = "opacity 0.75";
+                        command = "opacity 1";
                         criteria.class = ".*";
                     }
                     {
