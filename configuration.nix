9e88a7298b25662dd5cdb205b13f2b96456aad15
soma <ligma@mailbox.org>
2026-08-01 23:19:38 +0200

    You commented out several Newsboat color and macro settings and removed the `cleanup-on-quit` option

--- configuration.nix
+++ configuration.nix
@@ -999,17 +999,16 @@
             enable = true;
             browser = "/etc/profiles/per-user/soma/bin/mpv";
             extraConfig = ''
-                color listfocus black white
-                color listfocus_unread black white bold
-                color title black black
-                color info black black
-                macro , open-in-browser
-                ignore-mode "display"
+                #color listfocus black white
+                #color listfocus_unread black white bold
+                #color title black black
+                #color info black black
+                #macro , open-in-browser
+                #ignore-mode "display"
                 ignore-article "*" "title =~ \"#shorts\""
                 #ignore-article "*" "description =~ \"#shorts\""
                 #ignore-article "*" "content =~ \"#shorts\""
                 ignore-article "*" "link =~ \"shorts\""
-                cleanup-on-quit yes
                 macro a set browser "yt-dlp --write-auto-sub -q --no-warnings --skip-download -o /tmp/sub %u ; cat /tmp/sub.en.vtt| sed -e '/^[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{3\} -->/d' -e '/^[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{3\}/d' -e 's/<[^>]*>/g'| awk 'NF'| sed 's/$/ /'| tr -d '\n'| aichat 'give a detailed summary of the previous text with the main points. Do not mention any promotions or sponsors.'|less";open-in-browser;set browser mpv'';
         };
         programs.btop = {
