698954f24e957f3433c87216f84cfb0fbb799aad
none <none@none>
2026-08-02 23:33:42 +0200

    GTK and Qt theme and package assignments were commented out, disabling Adwaita-dark theming for both

--- configuration.nix
+++ configuration.nix
@@ -858,14 +858,14 @@
         gtk = {
             enable = true;
             colorScheme = "dark";
-            theme.name = "Adwaita-dark";
-            theme.package = pkgs.gnome-themes-extra;
+            #theme.name = "Adwaita-dark";
+            #theme.package = pkgs.gnome-themes-extra;
         };
         qt = {
             enable = true;
             platformTheme.name = "gtk";
-            style.name = "Adwaita-dark";
-            style.package = pkgs.adwaita-qt;
+            #style.name = "Adwaita-dark";
+            #style.package = pkgs.adwaita-qt;
         };
         #dconf.settings."org/freedesktop/appearance".color-scheme = "1";
         dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
