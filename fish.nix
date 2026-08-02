75f18fc08360ecc3e5d089f3f7fba3b55615ea8e
soma <ligma@mailbox.org>
2026-08-02 05:10:26 +0200

    Reordered `fzf` and `rem` aliases, removed the `rc3` alias, and changed the `fzf` command to use the full path, set `SHELL=bash`, and added `FZF_DEFAULT_COMMAND='fd --type f --type l --type d --strip-cwd-prefix'`

--- fish.nix
+++ fish.nix
@@ -302,9 +302,9 @@
             #zathura = "swallow zathura-sandbox";
             zathura = "zathura-sandbox";
             rc = "rem -cumb1";
-            rc3 = "rem -cu3mb1";
             rc2 = "rem -cu2mb1";
-            fzf = "SHELL=bash fzf --preview 'fzf-preview {}' 2>/dev/null";
+            rc3 = "rem -cu3mb1";
+            fzf = "SHELL=bash FZF_DEFAULT_COMMAND='fd --type f --type l --type d --strip-cwd-prefix' /run/current-system/sw/bin/fzf --preview 'fzf-preview {}' 2>/dev/null";
             rw = "rem -c+um";
             "rec" = "pactl set-source-volume @DEFAULT_SOURCE@ 50% ; /run/current-system/sw/bin/rec -c 1 /home/soma/dx/Recordings/$(date \"+%Y-%m-%d %H.%M.%S\").ogg";
             irec = "ffmpeg -ac 1 -f pulse -i record_sink.monitor /home/soma/dx/Recordings/$(date \"+%Y-%m-%d %H.%M.%S\").ogg";
