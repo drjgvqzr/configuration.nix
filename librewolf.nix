612c99cd3000826b17fb9fa71a69cbf85e2924af
none <none@none>
2026-08-03 03:32:34 +0200

    Removed unused `config` and `lib` parameters, tightened the function header, and deleted the commented-out `package` line

--- librewolf.nix
+++ librewolf.nix
@@ -1,9 +1,4 @@
-{
-    config,
-    pkgs,
-    lib,
-    ...
-}: {
+{pkgs, ...}: {
     home-manager.users.soma.programs.librewolf = {
         enable = true;
         #package = pkgs.librewolf-bin;
