3507e2e86f855d168a4d2261b0e930f51cfdb4b9
none <none@none>
2026-08-03 23:36:36 +0200

    Replaced a multi-pipe clipboard text processing command with a simplified pipeline that removes newlines and collapses spaces

--- sway.nix
+++ sway.nix
@@ -42,7 +42,7 @@ in {
                 };
             };
             keybindings = {
-                "Ctrl+Shift+v" = ''exec wl-paste | tr -d '-\n' | sed 's/ \n/ /g' | wl-copy'';
+                "Ctrl+Shift+v" = "wl-paste | tr '\\n' ' ' | wl-copy";
                 "Pause" = "exec playerctl --player mpv play-pause || playerctl play-pause";
                 "Print" = "exec grim -g \"$(slurp)\"";
                 "Shift+Print" = "exec grim -g \"$(slurp)\" - | tesseract - - | wl-copy";
