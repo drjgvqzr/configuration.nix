891b2a2a15651992f154a1b4ff44047077b19284
none <none@none>
2026-08-07 07:34:29 +0200

    Removed /home/soma/vs from borgmatic backup source directories

--- configuration.nix
+++ configuration.nix
@@ -308,7 +308,6 @@ in {
                     "/home/soma/px"
                     "/home/soma/ph"
                     "/home/soma/dn"
-                    "/home/soma/vs"
                     "/home/soma/.ssh"
                     "/home/soma/.gnupg"
                     "/home/soma/.config"
