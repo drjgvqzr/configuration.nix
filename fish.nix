8b70330bdf71d2337a394fe1b6fcbab4c44770d0
none <none@none>
2026-09-25 12:46:13 +0200

    Added an `nbv` alias for editing the newsboat config and commented out the two reminders printed on fish shell init

--- fish.nix
+++ fish.nix
@@ -217,6 +217,7 @@ in {
             nf = "fastfetch";
             b = "btop";
             nb = "newsboat";
+            nbv = "vi ~/dx/nixos/misc/newsboat";
             lb = "lsblk";
             qr = "qrrs";
             wq = "wl-paste | xargs -I {} qrrs {}";
@@ -277,9 +278,9 @@ in {
             trashempty = "gtrash prune --day 0";
         };
         shellInit = ''
-            rem -n -b1 | sort -r | tail -n 3 | sed 's|^[0-9]\{4\}/||'
+            #rem -n -b1 | sort -r | tail -n 3 | sed 's|^[0-9]\{4\}/||'
             echo -e "\033[31m$(date '+%m/%d %R %A') \033[91m$(echo "scale=5; ($(date +%s)-$(date -d"$(cat ${nixos}/misc/secrets/birthdate)" +%s))/(80*365.2425*86400)*100"|bc|sed 's/0*$//')%\033[0m \033[92m$(cat /tmp/webn)\033[0m"
-            remind ~/dx/Backups/remind/chores.rem | tail -n +2 | grep -v '^$'
+            #remind ~/dx/Backups/remind/chores.rem | tail -n +2 | grep -v '^$'
 
             set fish_greeting
             fish_default_key_bindings
